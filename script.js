f92eaf5ba5ca9fe6cfe186f1b1c9d9f8ca6be608
grigsamurainambut2023-a11y <grigsamurainambut.2023@gmail.com>
2026-09-19 02:34:02 +0700

    Add button interaction in Hero Section

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menambahkan interaksi pada tombol di Hero Section
+document.getElementById('btnSapa').addEventListener('click', function() {
+    alert('Halo! Terima kasih sudah mengunjungi portofolio saya.');
+});
